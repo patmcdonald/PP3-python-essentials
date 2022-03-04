e2d2fe9ff2e3b27f9bc1f83521962d8a4bef7cab
Pat McDonald <patmcdonald@me.com>
2022-03-04 12:54:41 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,39 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# Battleship command line game
+
+# declare gameboard variable
+gameboard = []
+for x in range(0, 8):
+    gameboard.append(['w'] * 8)
+
+"""
+Prints the Battleship gameboard
+"""
+def print_gameboard(gameboard):
+    for row in gameboard:
+        print(" ").join(row)
+
+    print(gameboard)
+
+# ship positions on board
+def load_ships():
+    pass
+# input for gameplay - validated
+def player_input():
+    pass
+# hit/miss alert
+def alert():
+    pass
+
+# game score and results
+def score():
+    pass
+# main method
+if __name__ == '__main__':
+
+    print_gameboard(gameboard)
+
+    main()
