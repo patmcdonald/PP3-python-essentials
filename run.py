5926830b79945550ce43a614f1b0991f22d395e2
Pat McDonald <patmcdonald@me.com>
2022-03-04 13:56:12 +0000

    comment out intiial game, try single row game to start

--- run.py
+++ run.py
@@ -3,37 +3,59 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 # Battleship command line game
+# import randint module
+from random import randint
 
-# declare gameboard variable
-gameboard = []
-for x in range(0, 8):
-    gameboard.append(['w'] * 8)
+# single row game to start - keep it simple and build up..
+battleship = randint(0, 7)
+board = ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w']
 
-"""
-Prints the Battleship gameboard
-"""
-def print_gameboard(gameboard):
-    for row in gameboard:
-        print(" ").join(row)
+print("BATTLESHIP!")
 
-    print(gameboard)
+shot = 1
+while shot < 5:
+    print(board)
+    guess = int(input("Guess where the Enemy Ship is: "))
+    if guess == battleship:
+        print("Direct Hit!")
+        break
+    else:
+        print("You missed! RELOAD!")
+        board[guess - 1] = "X"
+        shot = shot + 1
 
-# ship positions on board
-def load_ships():
-    pass
-# input for gameplay - validated
-def player_input():
-    pass
-# hit/miss alert
-def alert():
-    pass
+# Code below parked for now! VVV
 
-# game score and results
-def score():
-    pass
-# main method
-if __name__ == '__main__':
+# # declare gameboard variable
+# gameboard = []
+# for x in range(0, 8):
+#     gameboard.append(['w'] * 8)
 
-    print_gameboard(gameboard)
+# """
+# Prints the Battleship gameboard
+# """
+# def print_gameboard(gameboard):
+#     for row in gameboard:
+#         print(" ").join(row)
 
-    main()
+#     print(gameboard)
+
+# # ship positions on board
+# def load_ships():
+#     pass
+# # input for gameplay - validated
+# def player_input():
+#     pass
+# # hit/miss alert
+# def alert():
+#     pass
+
+# # game score and results
+# def score():
+#     pass
+# # main method
+# if __name__ == '__main__':
+
+#     print_gameboard(gameboard)
+
+#     main()
